424201b5c92a4ba2ae53cc68ef2676eebc13a915
JackCollett <jack.collett@live.co.uk>
2025-06-09 14:28:59 +0100

    added validation for input

--- SpringBoard-JackCollett/obj/Debug/net9.0/SpringBoard-JackCollett.AssemblyInfo.cs
+++ SpringBoard-JackCollett/obj/Debug/net9.0/SpringBoard-JackCollett.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpringBoard-JackCollett")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d517473ca520d1e556ca7fb97b1ceb687a4884f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da093e7c18909fd97ef073066bba3cfb0a4e82d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpringBoard-JackCollett")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpringBoard-JackCollett")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
